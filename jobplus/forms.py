5ee30449f00aff767ee400328f2ae1cfd476ddd2
wuqize <wuqize5109@qq.com>
2017-12-22 01:58:00 +0800

    fix #24 #25 #26 登陆注册页面，个人和企业信息配置

---
+++ jobplus/forms.py
@@ -0,0 +1,151 @@
+#coding=utf-8
+
+import re
+
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import TextAreaField, IntegerField
+
+from wtforms import ValidationError
+from wtforms.validators import Length, Email, EqualTo, DataRequired, URL, NumberRange, Optional
+
+from jobplus.models import User, db, Company, Job
+
+class RegisterForm(FlaskForm):
+    """ 注册
+    """
+
+    # 默认角色是用户
+    role = 10
+
+    username = StringField("用户名", validators=[DataRequired("请输入用户名。"),
+                                              Length(3,24, message="用户名长度要在3~24个字符之间。"),
+                                              Optional(strip_whitespace=True)])
+
+    email = StringField("邮箱", validators=[DataRequired("请输入邮箱。"),
+                                          Email(message="请输入合法的email地址。")])
+
+    password = PasswordField("密码", validators=[DataRequired("请输入密码。"),
+                                               Length(6, 24, message="密码长度要在6~24个字符之间。"),
+                                               Optional(strip_whitespace=True)
+                                               ])
+
+    repeat_password = PasswordField("重复密码", validators=[DataRequired("请确认密码。"),
+                                                        EqualTo("password"),
+                                                        Optional(strip_whitespace=True)
+                                                        ])
+    submit = SubmitField("提交")
+
+    def create_user(self):
+        """创建用户
+        """
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        user.role = self.role
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def validate_username(self, field):
+        if len(re.sub("[0-9a-zA-Z_]", "", field.data)) != 0:
+            raise ValidationError("用户名只能包含数字、字母、下划线。")
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("用户名已经存在。")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
+
+
+class CompanyRegisterForm(RegisterForm):
+    """ 企业注册
+    """
+
+    # 角色是企业
+    role = 20
+
+    username = StringField("企业名称", validators=[DataRequired("请输入用户名。"),
+                                              Length(3, 24, message="用户名长度要在3~24个字符之间。"),
+                                              Optional(strip_whitespace=True)])
+
+class LoginForm(FlaskForm):
+
+    email = StringField("邮箱", validators=[DataRequired(message="请输入邮箱。"),
+                                          Email(message="邮箱格式不正确。"),
+                                          Optional(strip_whitespace=True)])
+
+    password = PasswordField("密码", validators=[DataRequired("请输入密码。"),
+                                               Length(6, 24),
+                                               Optional(strip_whitespace=True)])
+    remember_me = BooleanField("记住我")
+    submit = SubmitField("提交")
+
+    def validate_email(self, field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError("邮箱未注册。")
+
+    def validate_password(self, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError("密码错误。")
+
+
+class UserProfileForm(FlaskForm):
+    real_name = StringField("姓名")
+    email = StringField("邮箱", validators=[DataRequired(), Email()])
+    password = PasswordField("密码(不填写保持不变)")
+    phone = StringField("手机号")
+    work_years = IntegerField("工作年限")
+    resume_url = StringField("简历地址")
+    submit = SubmitField("提交")
+
+    def validate_phone(self, field):
+        phone = field.data
+        if not re.match("^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$", phone):
+            raise ValidationError('请输入有效的手机号。')
+
+    def updated_profile(self, user):
+        user.real_name = self.real_name.data
+        user.email = self.email.data
+        if self.password.data:
+            user.password = self.password.data
+        user.phone = self.phone.data
+        user.work_years = self.work_years.data
+        user.resume_url = self.resume_url.data
+        db.session.add(user)
+        db.session.commit()
+
+class CompanyProfileForm(FlaskForm):
+    name = StringField("企业名称")
+    email = StringField("邮箱", validators=[DataRequired(message="请输入邮箱。"), Email(message="邮箱格式不正确。")])
+    password = PasswordField("密码(不填写保持不变)")
+    slug = StringField("Slug", validators=[DataRequired(""), Length(3, 24, message="不要太长，也不要太短(3, 24)。")])
+    location = StringField("地址", validators=[Length(0, 64)])
+    site = StringField("公司网站", validators=[Length(0, 64)])
+    logo = StringField("Logo")
+    description = StringField("一句话描述", validators=[Length(0, 100)])
+    about = TextAreaField("公司详情", validators=[Length(0, 1024)])
+    submit = SubmitField("提交")
+
+    def validate_phone(self, field):
+        phone = field.data
+        if not re.match("^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$", phone):
+            raise ValidationError('请输入有效的手机号。')
+
+    def updated_profile(self, user):
+        user.username = self.name.data
+        user.email = self.email.data
+        if self.password.data:
+            user.password = self.password.data
+        if user.company:
+            company = user.company
+        else:
+            company = Company()
+            company.user_id = user.id
+        self.populate_obj(company)
+        db.session.add(user)
+        db.session.add(company)
+        db.session.commit()
--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -1,3 +1,24 @@
-from flask import Blueprint
+#coding=utf-8
+
+from flask import Blueprint, render_template
+from flask import redirect, flash, url_for
+from flask_login import login_required, current_user
+
+from jobplus.forms import CompanyProfileForm
+from jobplus.decorators import company_required
 
 company = Blueprint('company', __name__, url_prefix='/companies')
+
+
+@company.route('/profile', methods=['GET', 'POST'])
+@login_required
+@company_required
+def profile():
+    form = CompanyProfileForm(obj=current_user.company)
+    form.name.data = current_user.username
+    form.email.data = current_user.email
+    if form.validate_on_submit():
+        form.updated_profile(current_user)
+        flash('企业信息更新成功', 'success')
+        return redirect(url_for('front.index'))
+    return render_template('company/profile.html', form=form)
